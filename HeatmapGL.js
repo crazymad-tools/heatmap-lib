942ff4fef69dd7af90ec184cf69a1cf625c71cc5
crazy_mad <2116913961@qq.com>
2022-01-21 11:32:51 +0800

    webgl 密度图生成

--- HeatmapGL.js
+++ HeatmapGL.js
@@ -3,6 +3,13 @@ function isDefined(val) {
 }
 
 class ShadowProgram extends WebglProgram {
+  constructor(gl) {
+    super(gl);
+
+    this.drawCircle();
+    this.drawGradient();
+  }
+
   drawCircle() {
     const canvas = document.createElement("canvas");
     const context = canvas.getContext("2d");
@@ -11,6 +18,7 @@ class ShadowProgram extends WebglProgram {
     canvas.height = 60;
 
     // context.fillStyle = "black";
+    context.clearRect(0, 0, 60, 60);
 
     context.shadowOffsetX = context.shadowOffsetY = 60;
     context.shadowBlur = 15;
@@ -21,7 +29,9 @@ class ShadowProgram extends WebglProgram {
     context.closePath();
     context.fill();
 
-    this.circle = canvas;
+    // this.circle = canvas;
+    this.circle = new Image();
+    this.circle.src = canvas.toDataURL();
   }
 
   drawGradient() {
@@ -42,8 +52,138 @@ class ShadowProgram extends WebglProgram {
     this.gradient = canvas;
   }
 
-  render() {
+  createRect(x1, y1) {
+    const x2 = x1 + 100;
+    const y2 = y1 + 100;
 
+    return new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]);
+  }
+
+  createAndSetupTexture() {
+    const gl = this.gl;
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+
+    // 设置材质，这样我们可以对任意大小的图像进行像素操作
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+
+    return texture;
+  }
+
+  createFrameBuffer() {
+    const gl = this.gl;
+    var texture = createAndSetupTexture(gl);
+    textures.push(texture);
+
+    // 设置纹理大小和图像大小一致
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      gl.canvas.width,
+      gl.canvas.height,
+      0,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      null
+    );
+
+    const fbo = gl.createFramebuffer();
+
+    framebuffers.push(fbo);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fbo);
+
+    // 绑定纹理到帧缓冲
+    gl.framebufferTexture2D(
+      gl.FRAMEBUFFER,
+      gl.COLOR_ATTACHMENT0,
+      gl.TEXTURE_2D,
+      texture,
+      0
+    );
+  }
+
+  renderOneShadow(x, y) {
+    const gl = this.gl;
+    gl.blendFunc(gl.ONE, gl.ONE);
+
+    gl.enable(gl.BLEND);
+
+    const program = this.program;
+
+    // 片元裁切范围
+    this.bindBufferData("a_position", this.createRect(x, y));
+
+    // 权重
+    const weightLocation = gl.getUniformLocation(program, "u_weight");
+
+    this.enableVertextAttrib("a_position", {
+      size: 2,
+      type: gl.FLOAT,
+      normalize: false,
+      stride: 0,
+      offset: 0,
+    });
+
+    gl.uniform1f(weightLocation, Math.random());
+
+    // Draw the rectangle.
+    const primitiveType = gl.TRIANGLES;
+    const offset = 0;
+    const count = 6;
+    gl.drawArrays(primitiveType, offset, count);
+
+    gl.disable(gl.BLEND);
+  }
+
+  render() {
+    // this.renderOneShadow(60, 60);
+    const gl = this.gl;
+    const program = this.program;
+
+    // 图像裁切范围
+    this.bindBufferData(
+      "a_texCoord",
+      new Float32Array([
+        0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0,
+      ]),
+      gl.STATIC_DRAW
+    );
+
+    // 坐标变换
+    const resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+    gl.useProgram(program);
+
+    gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+
+    this.enableVertextAttrib("a_texCoord", {
+      size: 2,
+      type: gl.FLOAT,
+      normalize: false,
+      stride: 0,
+      offset: 0,
+    });
+
+    const texture = this.createAndSetupTexture();
+    // 将图像上传到纹理
+    gl.texImage2D(
+      gl.TEXTURE_2D,
+      0,
+      gl.RGBA,
+      gl.RGBA,
+      gl.UNSIGNED_BYTE,
+      this.circle
+    );
+
+    for (let i = 0; i < 100; i++) {
+      const x = Math.floor(Math.random() * 800);
+      const y = Math.floor(Math.random() * 600);
+
+      this.renderOneShadow(x, y);
+    }
   }
 }
 
@@ -70,6 +210,7 @@ class HeatmapGL {
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
     gl.clearColor(0, 0, 0, 0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.BLEND);
 
     for (const program of this.programs) {
       program.render();
--- heatmap.frag
+++ heatmap.frag
@@ -1,22 +1,15 @@
-// fragment shaders don't have a default precision so we need
-// to pick one. mediump is a good default
 precision mediump float;
 
+// our texture
+uniform sampler2D u_image;
+uniform float u_weight;
+
+// the texCoords passed in from the vertex shader.
+varying vec2 v_texCoord;
+
 void main() {
-    // gl_FragColor is a special variable a fragment shader
-    // is responsible for setting
-  gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+   vec4 color = texture2D(u_image, v_texCoord).bgra;
+   color.a = color.a * u_weight;
+
+   gl_FragColor = color;
 }
-
-// precision mediump float;
-
-// // 纹理
-// uniform sampler2D u_image;
-
-// // 从顶点着色器传入的纹理坐标
-// varying vec2 v_texCoord;
-
-// void main() {
-//    // 在纹理上寻找对应颜色值
-//   gl_FragColor = texture2D(u_image, v_texCoord);
-// }
